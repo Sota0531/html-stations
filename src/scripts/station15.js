9039e3bc9f41e522540990ebe27b63dfac67acc3
Sota0531 <na.3105.so@icloud.com>
2024-06-10 15:26:32 +0900

    s15.fin

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,4 +1,4 @@
-async function getData() {
+const getData = async() => {
   // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
@@ -6,11 +6,16 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+  const result = await new Promise ((resolve) => {
+    setTimeout(() => {
+      resolve(test(userList));
+    },3000);
+  });
+  return result.map(test);
 }
 
-function test() {
-  return
+function test(data) {
+  data.full_name = data.family_name + " " + data.first_name;
+  return data;
 }
 
